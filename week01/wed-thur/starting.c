0611a8783e71153f30c5b18e1e39c898bb88dc9a
steve <steve@hendrikse.ca>
2024-09-06 16:46:50 +0000

    first week material

---
+++ week01/wed-thur/starting.c
@@ -0,0 +1,18 @@
+
+
+// write a program to translate inches to cm.  get the inches from the 
+// user and print the cm to the screen
+
+// steveh - prog71990 - week1 - fall24
+
+
+
+//input
+// introduce the program 
+// take user input (in inches)
+
+//transform
+// do the math (inches * 2.54 = cm)
+
+//output
+// print the height in cm to the screen 
